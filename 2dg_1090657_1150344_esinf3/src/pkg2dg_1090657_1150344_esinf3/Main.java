ecd6dcf3ac77598a20a8c262836248bee24a7ebb
Raúl Correia <Ral Correia@frost-x220>
2017-11-23 16:26:56 +0000

    Init repo 3

---
+++ 2dg_1090657_1150344_esinf3/src/pkg2dg_1090657_1150344_esinf3/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2dg_1090657_1150344_esinf3;
+
+/**
+ *
+ * @author Raúl Correia <1090657@isep.ipp.pt>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
