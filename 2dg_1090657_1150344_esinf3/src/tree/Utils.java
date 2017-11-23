3d583d1fe5b43429f14f48f2ac55ae73c9432fa7
Raúl Correia <Ral Correia@frost-x220>
2017-11-23 17:52:36 +0000

    sortByBST

--- 2dg_1090657_1150344_esinf3/src/tree/Utils.java
+++ 2dg_1090657_1150344_esinf3/src/tree/Utils.java
@@ -1,4 +1,3 @@
-
 package tree;
 
 import java.util.ArrayList;
@@ -9,7 +8,16 @@ import java.util.List;
  * @author DEI-ESINF
  */
 public class Utils {
-    public static <E extends Comparable<E>> Iterable<E> sortByBST(List<E> listUnsorted){
-        throw new UnsupportedOperationException("Not supported yet.");
-    }    
+
+    public static <E extends Comparable<E>> Iterable<E> sortByBST(List<E> listUnsorted) {
+        BST<E> bst = new BST<>();
+        for (E el : listUnsorted) {
+            bst.insert(el);
+        }
+        List<E> lista_sorted = new ArrayList<>();
+        for (E el : bst.inOrder()) {
+            lista_sorted.add(el);
+        }
+        return lista_sorted;
+    }
 }
