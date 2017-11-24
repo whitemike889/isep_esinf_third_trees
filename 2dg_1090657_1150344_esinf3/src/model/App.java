81e8a31977f287f023cc2b06710ecb29f9621cf8
Raúl Correia <Ral Correia@frost-x220>
2017-11-24 15:21:23 +0000

    inserirPoligono

--- 2dg_1090657_1150344_esinf3/src/model/App.java
+++ 2dg_1090657_1150344_esinf3/src/model/App.java
@@ -12,9 +12,33 @@ import tree.AVL;
  * @author Raúl Correia <1090657@isep.ipp.pt>
  */
 public class App {
-    
-    AVL<Poligono> arvore_unidades;
-    AVL<Poligono> arvore_dezenas;
-    AVL<Poligono> arvore_centenas;
-    
+
+    private AVL<Poligono> arvore_unidades;
+    private AVL<Poligono> arvore_dezenas;
+    private AVL<Poligono> arvore_centenas;
+
+    public App() {
+        arvore_unidades = new AVL<>();
+        arvore_dezenas = new AVL<>();
+        arvore_centenas = new AVL<>();
+    }
+
+    public boolean inserirPoligono(Poligono p) {
+        if (p.getNumLados() < 10) {
+            arvore_unidades.insert(p);
+            return true;
+        } else {
+            if (p.getNumLados() < 100) {
+                arvore_dezenas.insert(p);
+                return true;
+            } else {
+                if (p.getNumLados() < 1000) {
+                    arvore_centenas.insert(p);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
