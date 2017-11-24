f5d14fca89ebee43653486b4aebde46ff5798b8d
Rui Ruibeiro [1150344] <1150344@isep.ipp.pt>
2017-11-24 15:27:03 +0000

    Ficheiro trabalho

--- 2dg_1090657_1150344_esinf3/src/model/Ficheiro.java
+++ 2dg_1090657_1150344_esinf3/src/model/Ficheiro.java
@@ -17,7 +17,6 @@ import java.util.Scanner;
  */
 public class Ficheiro {
 
-
     /**
      * Método que lê um ficheiro de texto génerico e retorna uma lista com o
      * conteúdo
@@ -44,127 +43,19 @@ public class Ficheiro {
     }                                                                           //Total O(n)
 
     /**
-     * Método para ler Personagens e as Suas Alianças Primeiro método a ser
-     * exeucutado, antes de ler os Locais
+     * Método para ler Poligonos
      *
      * @param nomeFicheiro - Nome do Ficheiro
-     * @param jg Controlo de Jogo
+     * @param app Aplicação
      */
-    public void lerPersonagensAliancas(String nomeFicheiro, ControloDoJogo jg) {
-        List<String> conteudoFich = lerFicheiro(nomeFicheiro);                  //O(n)
-        boolean lerPersonagens = false;                                         //O(1)
-        boolean lerAliancas = false;                                            //O(1)
+    public void lerPoligonos(String nomeFicheiro, App app) {
+        List<String> conteudoFich = lerFicheiro(nomeFicheiro);                          //O(n)
 
-        String linhaSplit[] = null;                                             //O(1)
-        for (String linha : conteudoFich) {                                     //O(n)
-            if (linha.equals(PERSONAGENS)) {                                    //O(1)
-                lerPersonagens = true;                                          //O(1)
-                continue;
-            }
-            if (linha.equals(ALIANCAS)) {                                       //O(1)
-                lerPersonagens = false;                                         //O(1)
-                lerAliancas = true;                                             //O(1)
-                continue;
-            }
-            if (lerPersonagens == true) {                                       //O(1)
-                linhaSplit = linha.split(",");                                  //O(1)
-                Personagem p = new Personagem(linhaSplit[0], Integer.parseInt(linhaSplit[1]));//O(1)
-                jg.adicionarPersonagem(p);                                      //O(1)
-                continue;
-            }
-            if (lerAliancas == true) {                                          //O(1)
-                final int CAMPO_PERS_A = 0;                                     //O(1)
-                final int CAMPO_PERS_B = 1;                                     //O(1)
-
-                final int CAMPO_TIPO_ALIANCA = 2;                               //O(1)
-                final int CAMPO_ALIANCA_FATOR_COMPATIBILIDADE = 3;              //O(1)
-                linhaSplit = linha.split(",");                                  //O(1)
-
-                String pers_a = linhaSplit[CAMPO_PERS_A];                       //O(1)
-                String pers_b = linhaSplit[CAMPO_PERS_B];                       //O(1)
-                Boolean tipoAlianca = Boolean.parseBoolean(linhaSplit[CAMPO_TIPO_ALIANCA]);//O(1)
-                Double fator_comp = Double.parseDouble(linhaSplit[CAMPO_ALIANCA_FATOR_COMPATIBILIDADE]);//O(1)
-                Personagem persA = null, persB = null;                          //O(1)
-                for (Personagem p : jg.devolverTodasPersonagens()) {            //O(V)
-                    if (pers_a.equals(p.getNome())) {                           //O(1)
-                        persA = p;                                              //O(1)
-                        continue;
-                    }
-                    if (pers_b.equals(p.getNome())) {                           //O(1)
-                        persB = p;                                              //O(1)
-                    }
-                    if (persA != null && persB != null) {                       //O(1)
-                        jg.adicionarAlianca(persA, persB, tipoAlianca, fator_comp);//O(1)
-                        break;
-                    }
-                }
-
-            }
+        String linhaSplit[];                                                            //O(1)
+        for (String linha : conteudoFich) {                                             //O(n)
+            linhaSplit = linha.split(";");                                              //O(1)
+            Poligono p = new Poligono(Integer.parseInt(linhaSplit[0]), linhaSplit[1]);  //O(1)
+            app.inserirPoligono(p);                                                     //O(1)
         }
-    }                                                                           //Total O(n*Vpers)
-
-    /**
-     * Método para ler Locais e as Suas Estradas Segundo método a ser
-     * exeucutado, após ler as Alianças
-     *
-     * @param nomeFicheiro - Nome do Ficheiro
-     * @param jg - Controlo de Jogo
-     */
-    public void lerLocais(String nomeFicheiro, ControloDoJogo jg) {
-        List<String> conteudoFich = lerFicheiro(nomeFicheiro);                  //O(n)
-        boolean lerLocais = false;                                              //O(1)
-        boolean lerCaminhos = false;                                            //O(1)
-
-        String linhaSplit[] = null;                                             //O(1)
-        for (String linha : conteudoFich) {                                     //O(n)
-            if (linha.equals(LOCAIS)) {                                         //O(1)
-                lerLocais = true;                                               //O(1)
-                continue;                                                       //O(1)
-            }
-            if (linha.equals(CAMINHOS)) {                                       //O(1)
-                lerLocais = false;                                              //O(1)
-                lerCaminhos = true;                                             //O(1)
-                continue;                                                       //O(1)
-            }
-            if (lerLocais == true) {                                            //O(1)
-                linhaSplit = linha.split(",");                                  //O(z)
-                Local l = new Local(linhaSplit[0], Integer.parseInt(linhaSplit[1]));//O(1)
-                if (linhaSplit.length >= 3) {                                   //O(1)
-                    Personagem p = jg.obterPersonagemPorNome(linhaSplit[2]);    //O(V)
-                    if (p != null) {                                            //O(1)
-                        l.setDono(p);                                           //O(1)
-                    }
-                }
-                jg.adicionarLocal(l);                                           //O(v^2)
-                continue;                                                       //O(1)
-            }
-            if (lerCaminhos == true) {                                          //O(1)
-                final int CAMPO_LOCAL_A = 0;                                    //O(1)
-                final int CAMPO_LOCAL_B = 1;                                    //O(1)
-                final int CAMPO_DIF_ESTRADA = 2;                                //O(1)
-                
-                linhaSplit = linha.split(",");                                  //O(m)
-
-                String string_local_a = linhaSplit[CAMPO_LOCAL_A];              //O(1)
-                String string_local_b = linhaSplit[CAMPO_LOCAL_B];              //O(1)
-
-                Local locala = null, localb = null;                             //O(1)
-                for (Local l : jg.devolverTodosLocais()) {                      //O(v)
-                    if (string_local_a.equals(l.getNome())) {                   //O(1)
-                        locala = l;
-                    }
-                    if (string_local_b.equals(l.getNome())) {                   //O(1)
-                        localb = l;                                             //O(1)
-                    }
-                    if (locala != null && localb != null) {                     //O(1)
-                        double e = Double.parseDouble(linhaSplit[CAMPO_DIF_ESTRADA]);//O(1)
-                        jg.adicionarEstrada(locala, localb, e);                 //O(V^2local)
-                        break; //break ao for
-                    }
-
-                }
-            }
-
-        }
-    }                                                                           //Total:O(n)*(O(z)+O(Vlocal^2)+O(m)+O(e)+(Vlocal^2)
-}                                                                               //Total: O(n*Vlocal^2)
+    }
+}
