c866052a65addedd0cb1fba4a8bcff3760d0e8fc
Raúl Correia <Ral Correia@frost-x220>
2017-11-24 14:47:43 +0000

    AVL

--- 2dg_1090657_1150344_esinf3/src/tree/AVL.java
+++ 2dg_1090657_1150344_esinf3/src/tree/AVL.java
@@ -10,52 +10,77 @@ package tree;
  * @author DEI-ESINF
  * @param <E>
  */
-public class AVL <E extends Comparable<E>> extends BST<E> {
-    
-    
-    private int balanceFactor(Node<E> node){
-        
+public class AVL<E extends Comparable<E>> extends BST<E> {
+
+    private int balanceFactor(Node<E> node) {
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    private Node<E> rightRotation(Node<E> node){
-        
+
+    private Node<E> rightRotation(Node<E> node) {
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    private Node<E> leftRotation(Node<E> node){
-        
+
+    private Node<E> leftRotation(Node<E> node) {
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    private Node<E> twoRotations(Node<E> node){
-        
+
+    private Node<E> twoRotations(Node<E> node) {
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    private Node<E> balanceNode(Node<E> node)
-    {
+
+    private Node<E> balanceNode(Node<E> node) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     @Override
-    public void insert(E element){
+    public void insert(E element) {
         root = insert(element, root);
     }
-    private Node<E> insert(E element, Node<E> node){
+
+    private Node<E> insert(E element, Node<E> node) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    @Override  
-    public void remove(E element){
+
+    @Override
+    public void remove(E element) {
         root = remove(element, root());
     }
 
     private Node<E> remove(E element, BST.Node<E> node) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (node == null) {
+            return null;
+        }
+        if (node.getElement().equals(element)) {
+            if (node.getLeft() == null && node.getRight() == null) {
+                return null;
+            }
+            if (node.getLeft() == null) {
+                return node.getRight();
+            }
+            if (node.getRight() == null) {
+                return node.getLeft();
+            }
+            E smallElement = smallestElement(node.getRight());
+            node.setElement(smallElement);
+            node.setRight(remove(smallElement, node.getRight()));
+            node = balanceNode(node);
+        } else {
+            if (node.getElement().compareTo(element) > 0) {
+                node.setLeft(remove(element, node.getLeft()));
+                node = balanceNode(node);
+            } else {
+                node.setRight(remove(element, node.getRight()));
+                node = balanceNode(node);
+            }
+
+        }
+        return node;
     }
-    
-    
+
     public boolean equals(AVL<E> second) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
@@ -63,5 +88,5 @@ public class AVL <E extends Comparable<E>> extends BST<E> {
     public boolean equals(Node<E> root1, Node<E> root2) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-  
+
 }
